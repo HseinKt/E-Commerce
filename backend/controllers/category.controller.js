35f8af70176cd3be56a12e70f87f09c12aac77c9
HseinKt <hseinkteish1998@gmail.com>
2025-07-31 22:02:33 +0300

    Created updateCategory function

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -6,8 +6,8 @@ exports.createCategory = async (req, res) => {
     try {
         const { name } = req.body;
 
-        const categoryExists = await Category.findOne({ name });
-        if( categoryExists ) return res.status(400).json({
+        const existingCategory = await Category.findOne({ name });
+        if( existingCategory ) return res.status(400).json({
             message: 'Category already exists'
         })
 
@@ -23,5 +23,41 @@ exports.createCategory = async (req, res) => {
     } catch (error) {
         console.error('Error creating category:', error);
         res.status(500).json({ message: 'Server error CREATING CATEGORY' });
+    }
+}
+
+exports.updateCategory = async (req, res) => {
+    console.log('Updating category:');
+
+    try {
+        const { name } = req.body;
+        const { id } = req.params.id;
+
+        const category = await Category.findById(id);
+        if( !category ) return res.status(404).json({
+            message: 'Category not found'
+        })
+
+        const existingCategory = await Category.findById(category);
+        if (!existingCategory) return res.status(400).json({
+            message: 'Category does not exist'
+        })
+
+        product.name = name || product.name;
+        product.description = description || product.description;
+        product.price = price || product.price;
+        product.quantity = quantity || product.quantity;
+        product.category = category || product.category;
+        product.image = image || product.image;
+
+        const updateProduct = await product.save();
+
+        res.status(201).json({
+            message: 'Product Updated successfully',
+            product: updateProduct
+        })
+    } catch (error) {
+        console.error('Error updating category:', error);
+        res.status(500).json({ message: 'Server error UPDATING CATEGORY' });
     }
 }
