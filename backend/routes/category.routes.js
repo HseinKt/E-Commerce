ad7f677438594d88c84e1e86c92f770897e8e4ce
HseinKt <hseinkteish1998@gmail.com>
2025-07-31 22:40:24 +0300

    tested category apis and fixed all errors

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
-const { createCategory } = require('../controllers/category.controller');
+const { getAllCategories, getCategoryById, createCategory, updateCategory, deleteCategory } = require('../controllers/category.controller');
 const { adminMiddleware } = require('../middleware/admin.middleware');
 const { authMiddleware } = require('../middleware/auth.middleware');
 
 const router = Router();
 
+router.get('/category', getAllCategories);
+router.get('/category/:id', getCategoryById);
 router.post('/category', createCategory);
+router.put('/category/:id', updateCategory);
+router.delete('/category/:id', deleteCategory);
 
 module.exports = router;
