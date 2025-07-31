910c3316c9a12b41fa45fa7ef9d66e5bc97ed7f9
HseinKt <hseinkteish1998@gmail.com>
2025-07-31 21:54:17 +0300

    Created getProductById function

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -19,6 +19,27 @@ exports.getAllProducts = async (req, res) => {
     }
 }
 
+exports.getProductById = async (req, res) => {
+    console.log('Fetching product by ID:');
+
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id).populate('category');
+
+        if (!product) return res.status(404).json({
+            message: 'Product not found'
+        })
+
+        res.status(200).json({
+            message: 'Product fetched successfully',
+            product: product
+        })
+    } catch (error) {
+        console.error('Error fetching product by ID:', error);
+        res.status(500).json({ message: 'Server error FETCHING PRODUCT BY ID' });
+    }
+}
+
 exports.createProduct = async (req, res) => {
     console.log('Creating product:');
 
