9346fbcadf1424c365dcc054c2431800c684669f
HseinKt <hseinkteish1998@gmail.com>
2025-08-02 18:02:00 +0300

    Created Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,39 @@
+import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+    const {user, logout} = useContext(AuthContext);
+
     return ( 
         <nav className="navbar">
-            Navbar
+            <h1 className='logo'>ROOTED</h1>
+            <div className='links'>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+
+                {user?.role === 'admin' && (
+                    <>
+                        <Link to="/admin/dashboard">Dashboard</Link>
+                        <button className='logout-button' onClick={logout}>Logout</button>
+                    </>
+                )}
+
+                {!user && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
+
+                {user?.role === 'user' && (
+                    <>
+                        <span className='user-greeting'>Hello, {user.name}</span>
+                        <button className='logout-button' onClick={logout}>Logout</button>
+                    </>
+                )}
+            </div>
         </nav>
     );
 }
