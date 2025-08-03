0a3164b486628cb5b4c61b10dae80c4cae313fa0
HseinKt <hseinkteish1998@gmail.com>
2025-08-04 02:09:19 +0300

    Created PrivateRouter for admin authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Register from './pages/Register';
 import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import NotFound from './pages/NotFound';
+import PrivateRoute from './routes/PrivateRoute';
+import Dashboard from './pages/Admin/Dashboard';
+import ProductManager from './pages/Admin/ProductManager';
+import CategoryManager from './pages/Admin/CategoryManager';
 
 function App() {
 
@@ -16,7 +20,25 @@ function App() {
         <Route path='/register' element={ <Register/> }/>
         <Route path='/products' element={ <Products/> }/>
         <Route path='/products/:id' element={ <ProductDetails/> }/>
-        <Route path='*' element={<NotFound />} />
+        <Route path='*' element={<NotFound/>} />
+
+        <Route path='/admin/dashboard' element={
+          <PrivateRoute allowedRoles={'admin'}>
+            <Dashboard/>
+          </PrivateRoute>
+        }/>
+
+        <Route path='/admin/products' element={
+          <PrivateRoute allowedRoles={'admin'}>
+            <ProductManager/>
+          </PrivateRoute>
+        }/>
+
+        <Route path='/admin/categories' element={
+          <PrivateRoute allowedRoles={'admin'}>
+            <CategoryManager/>
+          </PrivateRoute>
+        }/>
         
       </Routes>
     </>
