689cd525735000343c0cdd8d6503689b80fa30c9
HseinKt <hseinkteish1998@gmail.com>
2025-08-15 05:12:30 +0300

    Added category titles in product page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -7,6 +7,7 @@ import '../styles/Products.css'
 const Products = () => {
     const { token, logout } = useContext(AuthContext);
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
 
     const fetchProducts  = async () => {
         
@@ -32,33 +33,74 @@ const Products = () => {
         }
     }
 
+    const fetchCategories = async () => {
+
+        try {
+            axios.get("http://localhost:8000/api/category",
+            {
+                headers:
+                {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer '+ token
+                }
+            })
+            .then((response) => {
+                setCategories(response.data.categories)
+            })
+            .catch((error) => {
+                const errorMessage = error.response?.data?.message || "No fetch data found, catch error";
+                console.log("errorMessage in catch axios", errorMessage);
+                alert( errorMessage);
+            })
+        } catch (error) {
+            console.error("Error during fetching:", error);
+        }
+    }
+
     useEffect(() => {
         if(!token) 
             logout();
 
         fetchProducts();
+        fetchCategories();
     }, [])
 
     return ( 
         <div className="product-container">
+
             <h2>Healthy eating</h2>
 
-            <div className="grid">
-                {products.map(product => (
-                <Link to={`/products/${product._id}`} className="product-link" key={product._id}>
-                    <div className="product-card">
-                        <img src={`http://localhost:8000${product.image}?t=${Date.now()}`} alt={product.name} className="product-image"/>
-                        <p className="product-price">${product.price}</p>
-                        <h3 className="product-name">{product.name}</h3>
-                        <div className="stars">
-                            ★★★★★
+            {categories.map(category => {
+
+                const filteredProducts = products.filter( p => (
+                    p.category && (p.category._id === category._id)
+                ));
+                
+                if (filteredProducts.length === 0) return null; // skip empty categories
+
+                return (
+                    <div key={category._id} className="category-section">
+                        <h3 className="category-title">{category.name}</h3>
+
+                        <div className="grid">
+                            {filteredProducts.map(product => (
+                                <Link to={`/products/${product._id}`} className="product-link" key={product._id}>
+                                    <div className="product-card">
+                                        <img src={`http://localhost:8000${product.image}?t=${Date.now()}`} alt={product.name} className="product-image"/>
+                                        <p className="product-price">${product.price}</p>
+                                        <h3 className="product-name">{product.name}</h3>
+                                        <div className="stars">
+                                            ★★★★★
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))}
                         </div>
                     </div>
-                </Link>
-                ))}
-            </div>
+                );
+            })}
         </div>
-     );
+    );
 }
  
 export default Products;
